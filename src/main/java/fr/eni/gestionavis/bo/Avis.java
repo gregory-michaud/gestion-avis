aa664763677ef7dd891a80cb447fcae9ba5f9923
Gregory <gmichaud@campus-eni.fr>
2026-04-28 12:25:52 +0200

    M03_TP04

--- src/main/java/fr/eni/gestionavis/bo/Avis.java
+++ src/main/java/fr/eni/gestionavis/bo/Avis.java
@@ -1,10 +1,12 @@
 package fr.eni.gestionavis.bo;
 
+import fr.eni.gestionavis.bo.vin.Bouteille;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -28,4 +30,10 @@ public class Avis {
     private String commentaire;
 
     private LocalDateTime date;
+
+    private Client client;
+
+    @DBRef
+    @Field(name = "bottle_id")
+    private Bouteille  bouteille;
 }
--- src/test/java/fr/eni/gestionavis/association/TestAssociationAvisBouteille.java
+++ src/test/java/fr/eni/gestionavis/association/TestAssociationAvisBouteille.java
@@ -2,7 +2,9 @@ package fr.eni.gestionavis.association;
 
 import fr.eni.gestionavis.bo.Avis;
 import fr.eni.gestionavis.bo.Client;
+import fr.eni.gestionavis.bo.vin.Bouteille;
 import fr.eni.gestionavis.dal.AvisRepository;
+import fr.eni.gestionavis.dal.BouteilleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -10,20 +12,30 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.MethodName.class)
-public class TestAssociationAvisClient {
+public class TestAssociationAvisBouteille {
 
     @Autowired
     AvisRepository avisRepository;
 
+    @Autowired
+    BouteilleRepository bouteilleRepository;
+
     @Test
-    void test01_save_avis_CLient() {
+    void test01_save_avis_Bouteille() {
+        // Récupération des Bouteille en base
+        final List<Bouteille> listeBouteilleDB = bouteilleRepository.findAll();
+        assertThat(listeBouteilleDB).isNotNull();
+        assertThat(listeBouteilleDB).isNotEmpty();
+
+        final Bouteille bouteilleDB = listeBouteilleDB.get(0);
+
         final Client client = Client
                 .builder()
                 .pseudo("bobeponge@email.fr")
@@ -32,24 +44,28 @@ public class TestAssociationAvisClient {
 
         final Avis avis = Avis
                 .builder()
-                .note(4)
+                .note(5)
                 .commentaire("Doux. A déguster frais")
-                .date(LocalDateTime.now())
+                .client(client)
                 .build();
 
-        //Association
-        avis.setClient(client);
+        // Association avec Bouteille
+        avis.setBouteille(bouteilleDB);
 
+        // Sauver
         final Avis avisDB = avisRepository.save(avis);
 
         // Vérifier que l'identifiant n'est pas nul
         assertThat(avisDB.getId()).isNotNull();
         assertThat(avisDB.getId()).isNotBlank();
 
-        // Vérifier que la référence embarquée existe
-        final Client clientDB = avisDB.getClient();
-        assertThat(clientDB).isNotNull();
-        assertThat(clientDB).isEqualTo(client);
+        // Vérifier que le Client n'est pas nul
+        assertThat(avisDB.getClient()).isNotNull();
+        assertThat(avisDB.getClient()).isEqualTo(client);
+
+        // Vérifier que le Bouteille est complet
+        assertThat(avisDB.getBouteille().getId()).isNotNull();
+        assertThat(avisDB.getBouteille()).isEqualTo(bouteilleDB);
 
         log.info(avisDB.toString());
     }
